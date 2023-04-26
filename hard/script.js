db138340aafb54c9729ba4a135db99057987e3a0
Kevin Lam <Kevin@AdminisatorsMBP.attlocal.net>
2023-04-26 18:50:33 -0400

    final

--- hard/script.js
+++ hard/script.js
@@ -2,5 +2,14 @@
 // Tom - 	height:  9in   	mass: 8 g
 // Jerry - 	height: 10in 	mass: 45 g
 // Compare the BMI (Body Mass Index) of Tom & Jerry using the formula below:
-//     Mass=
-// Store Tom & Jerry’s mass and height as variables. Calculate both their BMI’s. Create a Boolean variable containing information about whether Tom has a higher BMI than Jerry. Print a string to the console with the variable from step 3 ( e.g. Is Tom’s BMI higher than Jerry’s? false).
+// Store Tom & Jerry’s mass and height as variables.
+var tomH = 9;
+var tomM = 8;
+var jerryH = 10;
+var jerryM = 45;
+// Calculating the BMI
+const tomBMI = tomM / (tomH / 12) ** 2;
+const jerryBMI = jerryM / (jerryH / 12) ** 2;
+//  Boolean variable containing information about whether Tom has a higher BMI than Jerry.
+const tomHasHigherBMI = tomBMI > jerryBMI;
+console.log(`Is Tom's BMI higher than Jerry's? ${tomHasHigherBMI}`);
